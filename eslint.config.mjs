6d948196b89106543d72f98fe2ef72f33b986231
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-27 15:24:25 -0300

    feat(eslint): create esling config   Configuring ESLint to validate code standardization. Prettier was installed and used as a plugin for ESLint; it is responsible for validating code formatting. A prettier-tailwindcss plugin was installed to organize the Tailwind classes. An ESLint simple-import-sort plugin was installed to standardize imports.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettier from 'eslint-plugin-prettier/recommended'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -8,11 +10,41 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+  prettier,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      // normal rules
+      semi: ['warn', 'never'], // Disallow semicolons at the end of statements
 
-export default eslintConfig;
+      quotes: ['warn', 'single'], // Enforce the use of single quotes for strings
+
+      indent: ['warn', 2], // Enforce consistent indentation of 2 spaces
+
+      'comma-dangle': ['warn', 'always-multiline'], // Require trailing commas in multiline objects, arrays, etc.
+
+      'max-len': ['warn', { code: 120 }], // Warn when a line exceeds 80 characters
+
+      'object-curly-spacing': ['warn', 'always'], // Require spaces inside curly braces (e.g., { foo: "bar" })
+
+      'arrow-parens': ['warn', 'always'], // Require parentheses around arrow function parameters
+
+      'jsx-quotes': ['warn', 'prefer-double'], // Enforce double quotes in JSX attributes
+
+      'simple-import-sort/imports': 'warn', // turn import sort to warn
+
+      'simple-import-sort/exports': 'warn', // turn export sort to warn
+
+      'prettier/prettier': 'warn', // turn prettier error in warning
+    },
+  },
+])
+
+export default eslintConfig
