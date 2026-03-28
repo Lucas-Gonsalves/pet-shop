db3d1959817cf4dcf2a0a8335b827924cd68ac8d
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-28 20:52:58 -0300

    feat(app): create main structure   The main layout structure and homepage have been created. Now the font variables are being applied correctly and the homepage is properly styled.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h2>Home Test</h2>
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between md:m-8">
+        <div>
+          <h1 className="text-title text-content-primary mb-2">Your schedule</h1>
+          <p className="text-paragraph-small text-content-secondary">
+            Here you can see all clients and services schedules for today
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
