ea131ab57e2dcfc8adba57b54002ec7d61464b62
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-03-29 20:22:37 -0300

    refactor(functions): correcting grammar errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,6 @@
-import { Appointment, AppointmentPeriod, AppointmentPeriodDay } from '@/@types/appointments'
 import { PeriodSection } from '@/components/period-section'
 import { appointments } from '@/utils/fake-data/appointments'
-
-import { Appointment as AppointmentPrisma } from './generated/prisma/client'
-
-const getPeriod = (hour: number): AppointmentPeriodDay => {
-  if (hour >= 9 && hour < 12) return 'morning'
-  if (hour >= 13 && hour < 18) return 'afternoon'
-  return 'evening'
-}
-
-function groupAppointmentByPeriod(appointments: AppointmentPrisma[]): AppointmentPeriod[] {
-  const transformedAppointments: Appointment[] = appointments?.map((apt) => ({
-    ...apt,
-    time: apt.scheduleAt.toLocaleTimeString('pt-BR', {
-      hour: '2-digit',
-      minute: '2-digit',
-    }),
-    service: apt.description,
-    period: getPeriod(apt.scheduleAt.getHours()),
-  }))
-
-  const morningAppointments = transformedAppointments.filter((apt) => apt.period === 'morning')
-  const afternoonAppointments = transformedAppointments.filter((apt) => apt.period === 'afternoon')
-  const eveningAppointments = transformedAppointments.filter((apt) => apt.period === 'evening')
-
-  return [
-    {
-      title: 'Morning',
-      type: 'morning',
-      timeRange: '09h-12h',
-      appointments: morningAppointments,
-    },
-    {
-      title: 'Tarde',
-      type: 'afternoon',
-      timeRange: '13h-18h',
-      appointments: afternoonAppointments,
-    },
-    {
-      title: 'Evening',
-      type: 'evening',
-      timeRange: '19h-21h',
-      appointments: eveningAppointments,
-    },
-  ]
-}
+import { groupAppointmentByPeriod } from '@/utils/functions/group-appointment-by-period'
 
 export default function Home() {
   const periods = groupAppointmentByPeriod(appointments)
