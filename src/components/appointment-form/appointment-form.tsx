1ddea736f78805a3d59db6fcec9d52810ee3f479
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-04-03 15:57:40 -0300

    feat(appointment-form): create mask input   The phone input was created using a mask from react-imask. It was then styled and positioned on the screen.

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { DogIcon, UserIcon } from 'lucide-react'
+import { DogIcon, PhoneIcon, UserIcon } from 'lucide-react'
 import { Controller, useForm } from 'react-hook-form'
+import { IMaskInput } from 'react-imask'
 import { z } from 'zod'
 
 import { Button } from '../ui/button'
@@ -111,6 +112,37 @@ export const AppointmentForm = () => {
                       aria-invalid={fieldState.invalid}
                       autoComplete="off"
                       className="pl-10"
+                    />
+                  </div>
+
+                  {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                </Field>
+              )}
+            />
+            <Controller
+              name="phone"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel
+                    htmlFor="form-appointment-phone"
+                    className="text-label-medium-size text-content-primary"
+                  >
+                    Phone
+                  </FieldLabel>
+                  <div className="relative">
+                    <PhoneIcon
+                      className="text-content-brand absolute top-1/2 left-3 -translate-y-1/2 transform"
+                      size={20}
+                    />
+                    <IMaskInput
+                      {...field}
+                      id="form-appointment-phone"
+                      mask="(00) 00000-0000"
+                      placeholder="(00) 00000-0000"
+                      aria-invalid={fieldState.invalid}
+                      autoComplete="off"
+                      className="border-border-primary bg-background-tertiary text-content-primary ring-offset-background placeholder:text-content-secondary focus-visible:ring-border-brand hover:border-border-secondary focus:border-border-brand focus-visible:border-border-brand aria-invalid:ring-destructive/20 aria-invalid:border-destructive flex h-12 w-full rounded-md border px-3 py-2 pl-10 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-1 focus-visible:ring-offset-0 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
                     />
                   </div>
 
